e4d4ad0b2e0b50763c6af2255ec526e523fbdeb5
rajan4405 <rajansharma@skillprompt.com>
2024-03-26 14:18:01 +0545

    SayHelloController.java file add

---
+++ myfirstwebapp/src/main/java/com/rajan/springboot/myfirstwebapp/hello/SayHelloController.java
@@ -0,0 +1,24 @@
+package com.rajan.springboot.myfirstwebapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SayHelloController {
+  // "say-hello" => "Hello! What are you learning today? "
+
+  @RequestMapping("say-hello")
+  @ResponseBody
+  public String sayHello() {
+    return "Hello! What are you learning today? ";
+  }
+
+  @RequestMapping("say-hello-html")
+  @ResponseBody
+  public String sayHelloHtml() {
+    StringBuffer sb = new StringBuffer();
+    sb.append("<html> <body><title>Hello</title><h1>Hello! HTML?</h1><body> </html>");
+    return sb.toString();
+  }
+}
